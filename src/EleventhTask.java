e11be20af1aaa724e624674de0ab01030fdc9120
Stonewall1 <impression97p@gmail.com>
2022-05-11 17:55:46 +0300

    EleventhTask

---
+++ src/EleventhTask.java
@@ -0,0 +1,7 @@
+public class EleventhTask {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            System.out.println((int)Math.pow(i , 2));
+        }
+    }
+}
